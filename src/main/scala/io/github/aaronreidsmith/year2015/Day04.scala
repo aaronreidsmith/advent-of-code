3021b9fd03265a0a0201ded20987c5d2b8d4d131
Aaron Smith <aaron.smith@ironnetcybersecurity.com>
2021-06-21 08:39:09 -0500

    Year 2015 Day 4

---
+++ src/main/scala/io/github/aaronreidsmith/year2015/Day04.scala
@@ -0,0 +1,33 @@
+package io.github.aaronreidsmith.year2015
+
+import java.math.BigInteger
+import java.security.MessageDigest
+
+object Day04 {
+  def main(args: Array[String]): Unit = {
+    val input        = "yzbqklnj"
+    val infiniteList = LazyList.from(1)
+
+    val part1 = infiniteList
+      .collectFirst {
+        case i if md5(s"$input$i").startsWith("00000") => i
+      }
+      .getOrElse(-1)
+    println(s"Part 1: $part1")
+
+    val part2 = infiniteList
+      .collectFirst {
+        case i if md5(s"$input$i").startsWith("000000") => i
+      }
+      .getOrElse(-1)
+    println(s"Part 2: $part2")
+  }
+
+  private def md5(input: String): String = {
+    val md     = MessageDigest.getInstance("MD5")
+    val digest = md.digest(input.getBytes)
+    val bigInt = new BigInteger(1, digest)
+    val hash   = bigInt.toString(16)
+    if (hash.length < 32) hash.reverse.padTo(32, '0').reverse else hash
+  }
+}
