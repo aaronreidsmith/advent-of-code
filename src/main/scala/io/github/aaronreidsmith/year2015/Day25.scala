dc165f75c58f6c2a33d2f4b7f11b3f8dc136d2d4
Aaron Smith <aaron.smith@ironnetcybersecurity.com>
2021-07-10 14:35:14 -0500

    Year 2015 Day 25

---
+++ src/main/scala/io/github/aaronreidsmith/year2015/Day25.scala
@@ -0,0 +1,17 @@
+package io.github.aaronreidsmith.year2015
+
+object Day25 {
+  def main(args: Array[String]): Unit = {
+    val inputRow = 2978
+    val inputCol = 3083
+
+    val index = {
+      val n        = inputRow + inputCol - 1
+      val topRight = n * (n + 1) / 2
+      topRight - inputRow
+    }
+    val ticket = Stream.iterate(20151125L)(ticket => (ticket * 252533) % 33554393).drop(index).head
+
+    println(s"Part 1: $ticket")
+  }
+}
