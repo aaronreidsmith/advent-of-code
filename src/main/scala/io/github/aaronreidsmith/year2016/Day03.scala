8998e3da9a3e8824fb6971aee189cb86844c2b79
Aaron Smith <aaron.smith@ironnetcybersecurity.com>
2021-07-11 09:30:39 -0500

    Year 2016 day 3

---
+++ src/main/scala/io/github/aaronreidsmith/year2016/Day03.scala
@@ -0,0 +1,27 @@
+package io.github.aaronreidsmith.year2016
+
+import scala.io.Source
+
+object Day03 {
+  def main(args: Array[String]): Unit = {
+    val input = Source.fromResource("2016/day03.txt")
+    val inputList = input
+      .getLines()
+      .map { line =>
+        val Array(a, b, c, _*) = line.trim.split("\\s+")
+        List(a.toInt, b.toInt, c.toInt)
+      }
+      .toList
+    input.close()
+
+    val part1 = inputList.count { case a :: b :: c :: _ => isTriangle(a, b, c) }
+    println(s"Part 1: $part1")
+
+    val part2 = inputList.transpose.foldLeft(0) { (acc, column) =>
+      acc + column.grouped(3).count { case a :: b :: c :: _ => isTriangle(a, b, c) }
+    }
+    println(s"Part 2: $part2")
+  }
+
+  private def isTriangle(a: Int, b: Int, c: Int): Boolean = (a + b) > c && (a + c) > b && (b + c) > a
+}
