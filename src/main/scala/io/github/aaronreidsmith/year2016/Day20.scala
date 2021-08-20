0f845307dbbcc9796e8656e61ed26dbae2753082
Aaron Smith <aaron.smith@ironnetcybersecurity.com>
2021-08-20 14:50:32 -0500

    Year 2016 Day 20

---
+++ src/main/scala/io/github/aaronreidsmith/year2016/Day20.scala
@@ -0,0 +1,29 @@
+package io.github.aaronreidsmith.year2016
+
+import scala.io.Source
+
+object Day20 {
+  def main(args: Array[String]): Unit = {
+    val input = Source.fromResource("2016/day20.txt")
+    val (starts, ends) = input
+      .getLines()
+      .map { line =>
+        val Array(start, end, _*) = line.split('-')
+        (start.toLong, end.toLong)
+      }
+      .toList
+      .unzip
+    input.close()
+
+    val sortedIntervals = (starts.sorted :+ 4294967296L, 0L :: ends.sorted).zipped
+
+    val part1 = sortedIntervals
+      .find { case (start, end) => start > end + 1 }
+      .map { case (_, end) => end + 1 }
+      .getOrElse(-1)
+    println(s"Part 1: $part1")
+
+    val part2 = sortedIntervals.foldLeft(0L) { case (acc, (start, end)) => acc + math.max(0, start - end - 1) }
+    println(s"Part 2: $part2")
+  }
+}
