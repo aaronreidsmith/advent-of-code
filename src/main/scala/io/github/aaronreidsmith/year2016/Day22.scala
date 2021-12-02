66ed5069f21745e01af49aa9e33f7d6892a92351
Aaron Smith <aaronreidsmith@gmail.com>
2021-12-02 08:49:55 -0600

    Year 2016 Day 22

--- src/main/scala/io/github/aaronreidsmith/year2016/Day22.scala
+++ src/main/scala/io/github/aaronreidsmith/year2016/Day22.scala
@@ -3,13 +3,17 @@ package io.github.aaronreidsmith.year2016
 import scala.io.Source
 
 object Day22 {
-  protected[this] case class Node(row: Int, col: Int, used: Int, available: Int) {
-    lazy val neighbors: Seq[(Int, Int)] = Seq((row + 1, col), (row - 1, col), (row, col + 1), (row, col - 1)).filter {
-      case (r, c) => 0 <= r && r <= 30 && 0 <= c && c <= 30 // 30 is hard-coded from the input
-    }
+  protected[this] case class Node(used: Int, available: Int) {
+    private val totalSpace = used + available
 
     def isEmpty: Boolean  = used == 0
     def nonEmpty: Boolean = !isEmpty
+
+    override def toString: String = {
+      val _used = if (used == 0) "__" else used
+      // High capacity nodes are basically walls since they are already full
+      if (totalSpace >= 500) "|||||" else s"${_used}/$totalSpace"
+    }
   }
 
   private val nodeEntry = "^/dev/grid/node-x(\\d+)-y(\\d+)\\s+\\d+T\\s+(\\d+)T\\s+(\\d+)T\\s+\\d+%$".r
@@ -20,7 +24,7 @@ object Day22 {
       line match {
         case nodeEntry(x, y, used, available) =>
           val (row, col) = (x.toInt, y.toInt)
-          acc + ((row, col) -> Node(row, col, used.toInt, available.toInt))
+          acc + ((row, col) -> Node(used.toInt, available.toInt))
         case other => throw new IllegalArgumentException(other)
       }
     }
@@ -33,5 +37,23 @@ object Day22 {
       } yield 1
     }.sum
     println(s"Part 1: $part1")
+
+    // Solved by hand like so: https://www.reddit.com/r/adventofcode/comments/5jor9q/comment/dbhvxkp/
+    // 62 steps to move hole to bottom left, then 29 cycles of 5 to move it to the top left. 29 * 5 + 62 = 207
+    printGrid(nodes)
+  }
+
+  private def printGrid(grid: Map[(Int, Int), Node]): Unit = {
+    var row = 0
+    grid.toSeq
+      .sortBy { case (position, _) => position }
+      .foreach {
+        case ((currRow, _), node) =>
+          if (currRow != row) {
+            row += 1
+            println()
+          }
+          print(s"$node ")
+      }
   }
 }
