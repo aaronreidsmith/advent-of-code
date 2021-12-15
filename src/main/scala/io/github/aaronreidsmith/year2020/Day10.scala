699f94b6f2667631fbdf652bab8d207257a71145
Aaron Smith <aaronreidsmith@gmail.com>
2021-12-15 11:33:00 -0600

    Year 2020 Day 10 to Scala

--- src/main/scala/io/github/aaronreidsmith/year2020/Day10.scala
+++ src/main/scala/io/github/aaronreidsmith/year2020/Day10.scala
@@ -3,18 +3,29 @@ package io.github.aaronreidsmith.year2020
 import io.github.aaronreidsmith.using
 
 import scala.annotation.tailrec
+import scala.collection.mutable
 
 object Day10 {
   def main(args: Array[String]): Unit = {
-    val adaptors      = using("2020/day10.txt")(_.getLines().toList.map(_.toInt).sorted)
-    val deviceJoltage = adaptors.last + 3
-    val joltageList   = 0 :: (adaptors :+ deviceJoltage)
-    println(s"Part 1: ${part1(joltageList)}")
-  }
+    val adaptors      = using("2020/day10.txt")(_.getLines().toVector.map(_.toInt).sorted)
+    val maxJoltage    = adaptors.last
+    val deviceJoltage = maxJoltage + 3
+    val joltages      = 0 +: adaptors :+ deviceJoltage
 
-  @tailrec
-  private def part1(joltageList: List[Int], differences: List[Int] = Nil): Int = joltageList match {
-    case _ :: Nil    => differences.count(_ == 1) * differences.count(_ == 3)
-    case a :: b :: _ => part1(joltageList.tail, (b - a) :: differences)
+    @tailrec
+    def part1(remaining: Vector[Int], differences: Vector[Int] = Vector()): Int = remaining match {
+      case Vector(_)        => differences.count(_ == 1) * differences.count(_ == 3)
+      case Vector(a, b, _*) => part1(remaining.tail, differences :+ (b - a))
+    }
+    println(s"Part 1: ${part1(joltages)}")
+
+    val cache = mutable.Map.empty[Int, Long]
+    def part2(currentJoltage: Int): Long = cache.getOrElseUpdate(
+      currentJoltage,
+      if (currentJoltage == maxJoltage) 1
+      else if (!joltages.contains(currentJoltage)) 0
+      else part2(currentJoltage + 1) + part2(currentJoltage + 2) + part2(currentJoltage + 3)
+    )
+    println(s"Part 2: ${part2(0)}")
   }
 }
