945bad1ab97d6454e78b1efb2e509713c098f0e3
Aaron Smith <aaronreidsmith@gmail.com>
2021-12-23 09:35:30 -0600

    Year 2021 Day 23

---
+++ src/main/scala/io/github/aaronreidsmith/year2021/Day23.scala
@@ -0,0 +1,9 @@
+package io.github.aaronreidsmith.year2021
+
+// TODO: I did today by hand. Maybe I will implement an algorithm in the future...
+object Day23 {
+  def main(args: Array[String]): Unit = {
+    println(s"Part 1: 18195")
+    println(s"Part 2: 50265")
+  }
+}
