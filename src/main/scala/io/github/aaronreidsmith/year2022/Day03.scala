d54ef61441973c5b0a11d8f24aca9a951b7fcca9
Aaron Smith <aaronreidsmith@gmail.com>
2022-12-03 07:55:44 -0600

    Optimize Dayr 2022 Day 3

--- src/main/scala/io/github/aaronreidsmith/year2022/Day03.scala
+++ src/main/scala/io/github/aaronreidsmith/year2022/Day03.scala
@@ -5,31 +5,26 @@ import io.github.aaronreidsmith.using
 import scala.io.Source
 
 object Day03 {
-  private implicit class CharOps(char: Char) {
-    def priority: Int = char.toInt - (if (char.isLower) 96 else 38)
-  }
-
   def main(args: Array[String]): Unit = {
     val input = using("2022/day03.txt")(parseInput)
     println(s"Part 1: ${part1(input)}")
     println(s"Part 2: ${part2(input)}")
   }
 
-  protected[year2022] def parseInput(file: Source): List[List[Int]] = {
-    file.getLines().toList.map(line => line.map(_.priority).toList)
+  protected[year2022] def parseInput(file: Source): List[String] = file.getLines().toList
+
+  protected[year2022] def part1(input: List[String]): Int = input.foldLeft(0) { (acc, rucksack) =>
+    val length        = rucksack.length
+    val (left, right) = rucksack.splitAt(length / 2)
+    val overlap       = left.intersect(right).head
+    acc + priority(overlap)
   }
 
-  protected[year2022] def part1(input: List[List[Int]]): Int = input.foldLeft(0) { (acc, rucksack) =>
-    val length  = rucksack.length
-    val left    = rucksack.take(length / 2).toSet
-    val right   = rucksack.drop(length / 2).toSet
-    val overlap = left.intersect(right).head
-    acc + overlap
-  }
-
-  protected[year2022] def part2(input: List[List[Int]]): Int = input.grouped(3).foldLeft(0) { (acc, triplet) =>
-    val a :: b :: c :: _ = triplet.map(_.toSet)
+  protected[year2022] def part2(input: List[String]): Int = input.grouped(3).foldLeft(0) { (acc, triplet) =>
+    val a :: b :: c :: _ = triplet
     val badge            = a.intersect(b).intersect(c).head
-    acc + badge
+    acc + priority(badge)
   }
+
+  private def priority(char: Char): Int = char.toInt - (if (char.isLower) 96 else 38)
 }
