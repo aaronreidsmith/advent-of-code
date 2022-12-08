4729647de4b9395b0a0f2712ed187d06a7f3b8da
Aaron Smith <aaronreidsmith@gmail.com>
2022-12-08 09:30:49 -0600

    Cleanup

--- src/main/scala/io/github/aaronreidsmith/year2022/Day08.scala
+++ src/main/scala/io/github/aaronreidsmith/year2022/Day08.scala
@@ -22,12 +22,13 @@ object Day08 {
 
   protected[year2022] def part1(input: SortedMap[Point, Int]): Int = input.foldLeft(0) {
     case (acc, (position, treeHeight)) =>
+      val (left, right, up, down) = treesOfInterest(input, position)
       if (
         isEdge(input, position) ||
-        treesToLeft(input, position).forall(_ < treeHeight) ||
-        treesToRight(input, position).forall(_ < treeHeight) ||
-        treesAbove(input, position).forall(_ < treeHeight) ||
-        treesBelow(input, position).forall(_ < treeHeight)
+        left.forall(_ < treeHeight) ||
+        right.forall(_ < treeHeight) ||
+        up.forall(_ < treeHeight) ||
+        down.forall(_ < treeHeight)
       ) {
         acc + 1
       } else {
@@ -36,47 +37,49 @@ object Day08 {
     case (acc, _) => acc
   }
 
-  protected[year2022] def part2(input: SortedMap[Point, Int]): Int = input.foldLeft(Int.MinValue) {
-    case (acc, (position, treeHeight)) if !isEdge(input, position) =>
-      val left = {
-        val all = treesToLeft(input, position).toVector.reverse
-        if (all.forall(_ < treeHeight)) all.size else all.takeWhile(_ < treeHeight).size + 1
-      }
-      val right = {
-        val all = treesToRight(input, position).toVector
-        if (all.forall(_ < treeHeight)) all.size else all.takeWhile(_ < treeHeight).size + 1
-      }
-      val up = {
-        val all = treesAbove(input, position).toVector.reverse
-        if (all.forall(_ < treeHeight)) all.size else all.takeWhile(_ < treeHeight).size + 1
-      }
-      val down = {
-        val all = treesBelow(input, position).toVector
-        if (all.forall(_ < treeHeight)) all.size else all.takeWhile(_ < treeHeight).size + 1
-      }
-      val scenicScore = left * right * up * down
-      if (scenicScore > acc) scenicScore else acc
-    case (acc, _) => acc
+  protected[year2022] def part2(input: SortedMap[Point, Int]): Int = {
+    def viewingDistance(trees: Vector[Int], height: Int): Int = if (trees.forall(_ < height)) {
+      trees.size
+    } else {
+      trees.takeWhile(_ < height).size + 1
+    }
+
+    input.foldLeft(Int.MinValue) {
+      case (acc, (position, treeHeight)) if !isEdge(input, position) =>
+        val (left, right, up, down) = treesOfInterest(input, position)
+        val scenicScore = {
+          viewingDistance(left.reverse, treeHeight) *
+            viewingDistance(right, treeHeight) *
+            viewingDistance(up.reverse, treeHeight) *
+            viewingDistance(down, treeHeight)
+        }
+        if (scenicScore > acc) scenicScore else acc
+      case (acc, _) => acc
+    }
   }
 
   private def isEdge(grid: SortedMap[Point, Int], position: Point): Boolean = {
     Seq(position.up, position.right, position.left, position.down).exists(point => !grid.contains(point))
   }
 
-  // TODO: There *has* to be a way to combine all of these into one
-  private def treesToLeft(grid: SortedMap[Point, Int], point: Point): Iterator[Int] = grid.iterator.collect {
-    case (other, height) if other != point && other.x == point.x && other.y < point.y => height
-  }
-
-  private def treesToRight(grid: SortedMap[Point, Int], point: Point): Iterator[Int] = grid.iterator.collect {
-    case (other, height) if other != point && other.x == point.x && other.y > point.y => height
-  }
-
-  private def treesAbove(grid: SortedMap[Point, Int], point: Point): Iterator[Int] = grid.iterator.collect {
-    case (other, height) if other != point && other.y == point.y && other.x < point.x => height
-  }
-
-  private def treesBelow(grid: SortedMap[Point, Int], point: Point): Iterator[Int] = grid.iterator.collect {
-    case (other, height) if other != point && other.y == point.y && other.x > point.x => height
+  private def treesOfInterest(
+      grid: SortedMap[Point, Int],
+      point: Point
+  ): (Vector[Int], Vector[Int], Vector[Int], Vector[Int]) = {
+    grid.foldLeft((Vector.empty[Int], Vector.empty[Int], Vector.empty[Int], Vector.empty[Int])) {
+      case ((left, right, up, down), (other, height)) =>
+        if (other != point && other.x == point.x && other.y < point.y) {
+          (left :+ height, right, up, down)
+        } else if (other != point && other.x == point.x && other.y > point.y) {
+          (left, right :+ height, up, down)
+        } else if (other != point && other.y == point.y && other.x < point.x) {
+          (left, right, up :+ height, down)
+        } else if (other != point && other.y == point.y && other.x > point.x) {
+          (left, right, up, down :+ height)
+        } else {
+          (left, right, up, down)
+        }
+      case (acc, _) => acc
+    }
   }
 }
