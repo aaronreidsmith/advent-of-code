9cf71cd901f8645b5a274a95819ed0037af07830
Aaron Smith <aaronreidsmith@gmail.com>
2022-12-16 09:56:12 -0600

    Dynamic main class

--- src/main/scala/io/github/aaronreidsmith/AdventOfCode.scala
+++ src/main/scala/io/github/aaronreidsmith/AdventOfCode.scala
@@ -1,185 +1,29 @@
 package io.github.aaronreidsmith
 
 import scala.annotation.tailrec
-import scala.collection.SortedMap
+import scala.reflect.runtime.universe
+import scala.util.{Failure, Success, Try}
 
 object AdventOfCode {
-  private val solutions2015: SortedMap[Int, Solution] = {
-    import year2015._
-    SortedMap(
-      1  -> Day01,
-      2  -> Day02,
-      3  -> Day03,
-      4  -> Day04,
-      5  -> Day05,
-      6  -> Day06,
-      7  -> Day07,
-      8  -> Day08,
-      9  -> Day09,
-      10 -> Day10,
-      11 -> Day11,
-      12 -> Day12,
-      13 -> Day13,
-      14 -> Day14,
-      15 -> Day15,
-      16 -> Day16,
-      17 -> Day17,
-      18 -> Day18,
-      19 -> Day19,
-      20 -> Day20,
-      21 -> Day21,
-      22 -> Day22,
-      23 -> Day23,
-      24 -> Day24,
-      25 -> Day25
-    )
-  }
-
-  private val solutions2016: SortedMap[Int, Solution] = {
-    import year2016._
-    SortedMap(
-      1  -> Day01,
-      2  -> Day02,
-      3  -> Day03,
-      4  -> Day04,
-      5  -> Day05,
-      6  -> Day06,
-      7  -> Day07,
-      8  -> Day08,
-      9  -> Day09,
-      10 -> Day10,
-      11 -> Day11,
-      12 -> Day12,
-      13 -> Day13,
-      14 -> Day14,
-      15 -> Day15,
-      16 -> Day16,
-      17 -> Day17,
-      18 -> Day18,
-      19 -> Day19,
-      20 -> Day20,
-      21 -> Day21,
-      22 -> Day22,
-      23 -> Day23,
-      24 -> Day24,
-      25 -> Day25
-    )
-  }
-  private val solutions2017: SortedMap[Int, Solution] = {
-    import year2017._
-    SortedMap(
-      1  -> Day01,
-      2  -> Day02,
-      3  -> Day03,
-      4  -> Day04,
-      5  -> Day05,
-      6  -> Day06,
-      7  -> Day07,
-      8  -> Day08,
-      9  -> Day09,
-      10 -> Day10,
-      11 -> Day11,
-      12 -> Day12,
-      13 -> Day13,
-      14 -> Day14,
-      15 -> Day15,
-      16 -> Day16,
-      17 -> Day17,
-      18 -> Day18,
-      19 -> Day19,
-      20 -> Day20,
-      21 -> Day21,
-      22 -> Day22,
-      23 -> Day23,
-      24 -> Day24,
-      25 -> Day25
-    )
-  }
-  private val solutions2018: SortedMap[Int, Solution] = {
-    import year2018._
-    SortedMap(
-      1  -> Day01,
-      2  -> Day02,
-      3  -> Day03,
-      4  -> Day04,
-      5  -> Day05,
-      6  -> Day06,
-      7  -> Day07,
-      8  -> Day08,
-      9  -> Day09,
-      10 -> Day10,
-      11 -> Day11,
-      12 -> Day12,
-      13 -> Day13,
-      14 -> Day14,
-      // TODO
-      17 -> Day17,
-      18 -> Day18,
-      19 -> Day19,
-      20 -> Day20,
-      21 -> Day21,
-      22 -> Day22,
-      // TODO
-      25 -> Day25
-    )
-  }
-  private val solutions2019: SortedMap[Int, Solution] = {
-    import year2019._
-    SortedMap(
-      1 -> Day01,
-      2 -> Day02,
-      // TODO
-    )
-  }
-  private val solutions2020: SortedMap[Int, Solution] = {
-    import year2020._
-    SortedMap(
-      1 -> Day01,
-      2 -> Day02,
-      3 -> Day03,
-      // TODO
-    )
-  }
-  private val solutions2021: SortedMap[Int, Solution] = SortedMap()
-  private val solutions2022: SortedMap[Int, Solution] = {
-    import year2022._
-    SortedMap(
-      1  -> Day01,
-      2  -> Day02,
-      3  -> Day03,
-      4  -> Day04,
-      5  -> Day05,
-      6  -> Day06,
-      7  -> Day07,
-      8  -> Day08,
-      9  -> Day09,
-      10 -> Day10,
-      11 -> Day11,
-      12 -> Day12,
-      13 -> Day13
-    )
-  }
-
-  private val allSolutions: SortedMap[Int, SortedMap[Int, Solution]] = SortedMap(
-    2015 -> solutions2015,
-    2016 -> solutions2016,
-    2017 -> solutions2017,
-    2018 -> solutions2018,
-    2019 -> solutions2019,
-    2020 -> solutions2020,
-    2021 -> solutions2021,
-    2022 -> solutions2022
-  )
+  private val runtimeMirror = universe.runtimeMirror(getClass.getClassLoader)
+  private val years         = 2015 to 2022
+  private val days          = 1 to 25
 
   def main(args: Array[String]): Unit = {
     val parsedArgs = parseArgs(args.toList)
     val yearToRun  = parsedArgs.get("year")
     val dayToRun   = parsedArgs.get("day")
     (yearToRun, dayToRun) match {
-      case (Some(year), Some(day)) => allSolutions(year)(day).run()
-      case (Some(year), None)      => allSolutions(year).values.foreach(_.run())
-      case (None, Some(day))       => allSolutions.values.foreach(year => year(day).run())
-      case (None, None)            => allSolutions.values.foreach(_.values.foreach(_.run()))
+      case (Some(year), Some(day)) => runSolution(year, day)
+      case (Some(year), None)      => days.foreach(runSolution(year, _))
+      case (None, Some(day))       => years.foreach(runSolution(_, day))
+      case (None, None) =>
+        for {
+          year <- years
+          day  <- days
+        } {
+          runSolution(year, day)
+        }
     }
   }
 
@@ -188,17 +32,28 @@ object AdventOfCode {
     case Nil => parsed
     case "--year" :: year :: tail =>
       val yearInt = year.toInt
-      val minYear = 2015
-      val maxYear = allSolutions.keys.max
       require(
-        minYear <= yearInt && yearInt <= maxYear,
-        s"Year must be in the following range: $minYear <= year <= $maxYear"
+        years.min <= yearInt && yearInt <= years.max,
+        s"Year must be in the following range: ${years.min} <= year <= ${years.max}"
       )
       parseArgs(tail, parsed.updated("year", yearInt))
     case "--day" :: day :: tail =>
       val dayInt = day.toInt
-      require(1 <= dayInt && dayInt <= 25, "Day must be in the following range: 1 <= day <= 25")
+      require(
+        days.min <= dayInt && dayInt <= days.max,
+        s"Day must be in the following range: ${years.min} <= day <= ${years.max}"
+      )
       parseArgs(tail, parsed.updated("day", dayInt))
     case other => throw new IllegalArgumentException(s"Unknown argument(s) supplied: $other")
+  }
+
+  // Dynamically load and run a solution class
+  private def runSolution(year: Int, day: Int): Unit = Try {
+    val solution  = runtimeMirror.moduleSymbol(Class.forName(f"io.github.aaronreidsmith.year$year.Day$day%02d$$"))
+    val runMethod = solution.typeSignature.members.filter(_.name.toString == "run").head.asMethod
+    runtimeMirror.reflect(runtimeMirror.reflectModule(solution).instance).reflectMethod(runMethod)()
+  } match {
+    case Success(_) => // Do nothing
+    case Failure(_) => println(s"Year $year day $day has not been converted to a sub-type of Solution")
   }
 }
