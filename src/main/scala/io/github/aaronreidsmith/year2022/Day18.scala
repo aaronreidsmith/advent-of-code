1578bcea8cdda967892f751f658d686c6a4f480e
Aaron Smith <aaronreidsmith@gmail.com>
2022-12-18 07:32:00 -0600

    Year 2022 Day 18

---
+++ src/main/scala/io/github/aaronreidsmith/year2022/Day18.scala
@@ -0,0 +1,58 @@
+package io.github.aaronreidsmith.year2022
+
+import io.github.aaronreidsmith.Solution
+import io.github.aaronreidsmith.annotations.Slow
+
+import scala.collection.mutable
+import scala.io.Source
+
+@Slow(part2 = true)
+object Day18 extends Solution(2022, 18) {
+  type I  = Set[Cube]
+  type O1 = Int
+  type O2 = Int
+
+  private[year2022] case class Cube(x: Int, y: Int, z: Int) {
+    def neighbors: Set[Cube] = Set(
+      this.copy(x = x - 1),
+      this.copy(x = x + 1),
+      this.copy(y = y - 1),
+      this.copy(y = y + 1),
+      this.copy(z = z - 1),
+      this.copy(z = z + 1)
+    )
+  }
+
+  override protected[year2022] def parseInput(file: Source): Set[Cube] = {
+    file.getLines().foldLeft(Set.empty[Cube]) { (acc, line) =>
+      val Array(x, y, z, _*) = line.split(',')
+      acc + Cube(x.toInt, y.toInt, z.toInt)
+    }
+  }
+
+  override protected[year2022] def part1(input: Set[Cube]): Int = {
+    input.foldLeft(0)((acc, cube) => acc + cube.neighbors.count(!input.contains(_)))
+  }
+
+  // Adapted from https://old.reddit.com/r/adventofcode/comments/zoqhvy/2022_day_18_solutions/j0oul0u/
+  override protected[year2022] def part2(input: Set[Cube]): Int = {
+    val (xs, ys, zs) = input.unzip3(cube => (cube.x, cube.y, cube.z))
+    // Have to pad our ranges for movement
+    val xRange = -1 to xs.max + 1
+    val yRange = -1 to ys.max + 1
+    val zRange = -1 to zs.max + 1
+
+    val seen = mutable.Set.empty[Cube]
+    val todo = mutable.Stack(Cube(-1, -1, -1))
+    while (todo.nonEmpty) {
+      val here = todo.pop()
+      val toVisit = here.neighbors.diff(input).diff(seen).filter { cube =>
+        xRange.contains(cube.x) && yRange.contains(cube.y) && zRange.contains(cube.z)
+      }
+      todo.pushAll(toVisit)
+      seen.add(here)
+    }
+
+    input.foldLeft(0)((acc, cube) => acc + cube.neighbors.count(seen.contains))
+  }
+}
