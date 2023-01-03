1275b46758fa4ea9678c792b8b21a2af5ff9d24b
Aaron Smith <aaronreidsmith@gmail.com>
2023-01-03 15:38:13 -0600

    Year 2019 Day 25 to Solution subtype

--- src/main/scala/io/github/aaronreidsmith/year2019/Day25.scala
+++ src/main/scala/io/github/aaronreidsmith/year2019/Day25.scala
@@ -1,12 +1,92 @@
 package io.github.aaronreidsmith.year2019
 
-import io.github.aaronreidsmith.using
-import net.fornwall.aoc.Solver
+import io.github.aaronreidsmith.{Solution, using}
 
-// TODO: Actually solve this
-object Day25 {
-  def main(args: Array[String]): Unit = {
-    val input = using("2019/day25.txt")(_.mkString)
-    println(s"Part 1: ${Solver.solve(2019, 25, 1, input)}")
+import scala.annotation.tailrec
+import scala.collection.mutable
+import scala.io.Source
+import scala.util.Try
+
+object Day25 extends Solution(2019, 25) {
+  type I  = IntCode
+  type O1 = Int
+  type O2 = Nothing
+
+  private implicit class IntCodeOps(intCode: IntCode) {
+    def describeSurroundings(printOutput: Boolean): Either[Int, IntCode] = {
+      @tailrec
+      def helper(computer: IntCode, message: String): Either[Int, IntCode] = {
+        if (message.endsWith("airlock.\"")) {
+          Left(message.filter(_.isDigit).toInt)
+        } else if (message.endsWith("Command?\n") || message.endsWith("!\n\n") || message.endsWith("\"\n")) {
+          if (printOutput) {
+            print(message)
+          }
+          Right(computer)
+        } else {
+          val nextComputer = computer.next
+          val nextMessage = nextComputer.result match {
+            case IntCode.Output(value) => message + value.toChar
+            case _                     => message
+          }
+          helper(nextComputer, nextMessage)
+        }
+      }
+
+      helper(intCode, "")
+    }
+  }
+
+  // Override this so if the user exits it doesn't output a wrong answer
+  override def run(): Unit = {
+    println("Year 2019, Day 25")
+    val input  = using("2019/day25.txt")(parseInput)
+    val answer = part1(input)
+    if (answer != -1) {
+      println(s"Part 1: $answer")
+    }
+  }
+
+  override protected[year2019] def parseInput(file: Source): IntCode = IntCode(file)
+
+  override protected[year2019] def part1(input: IntCode): Int = {
+    // Not a general solution; only works with my input. To play the game yourself, set `autoSolve` to `false` below
+    val commands = mutable.Queue(
+      "north",
+      "east",
+      "south",
+      "take hypercube",
+      "north",
+      "west",
+      "north",
+      "east",
+      "take tambourine",
+      "west",
+      "west",
+      "take spool of cat6",
+      "north",
+      "take weather machine",
+      "west",
+      "west",
+      "west",
+      "exit"
+    )
+
+    @tailrec
+    def helper(computer: IntCode, command: String, autoSolve: Boolean = true): Int = {
+      if (command != "exit") {
+        val input = (command :+ '\n').map(_.toLong)
+        computer.withInput(input: _*).describeSurroundings(!autoSolve) match {
+          case Left(password) => password
+          case Right(nextComputer) =>
+            val nextCommand = if (autoSolve) Try(commands.dequeue()).getOrElse("") else Console.in.readLine()
+            helper(nextComputer, nextCommand, autoSolve)
+        }
+      } else {
+        -1
+      }
+    }
+
+    helper(input, "")
   }
 }
