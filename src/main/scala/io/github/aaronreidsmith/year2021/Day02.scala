944e35861fac7b9964df45771c6f1968932046b8
Aaron Smith <aaronreidsmith@gmail.com>
2023-09-10 20:03:24 -0500

    Delete useless comment

--- src/main/scala/io/github/aaronreidsmith/year2021/Day02.scala
+++ src/main/scala/io/github/aaronreidsmith/year2021/Day02.scala
@@ -35,30 +35,4 @@ object Day02 extends Solution {
     }
     finalPos.x * finalPos.y
   }
-
-//  def main(args: Array[String]): Unit = {
-//    val input = Using.resource(Source.fromResource("2021/day02.txt"))(_.getLines().toList)
-//    println(s"Part 1: ${part1(input)}")
-//    println(s"Part 2: ${part2(input)}")
-//  }
-//
-//  private def part1(instructions: List[String]): Int = {
-//    val (finalRow, finalCol) = instructions.foldLeft((0, 0)) {
-//      case ((row, col), forward(x)) => (row, col + x.toInt)
-//      case ((row, col), down(x))    => (row + x.toInt, col)
-//      case ((row, col), up(x))      => (row - x.toInt, col)
-//      case (acc, _)                 => acc
-//    }
-//    finalRow * finalCol
-//  }
-//
-//  private def part2(instructions: List[String]): Int = {
-//    val (finalRow, finalCol, _) = instructions.foldLeft((0, 0, 0)) {
-//      case ((row, col, aim), forward(x)) => (row + aim * x.toInt, col + x.toInt, aim)
-//      case ((row, col, aim), down(x))    => (row, col, aim + x.toInt)
-//      case ((row, col, aim), up(x))      => (row, col, aim - x.toInt)
-//      case (acc, _)                      => acc
-//    }
-//    finalRow * finalCol
-//  }
 }
