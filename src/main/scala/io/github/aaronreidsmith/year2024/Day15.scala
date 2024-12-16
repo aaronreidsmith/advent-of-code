87d0a373f7a9c0f8a58eeeb0fcdb35b204017758
Aaron Smith <aaronreidsmith@gmail.com>
2024-12-15 20:53:42 -0600

    Year 2024 Day 15 part 2

--- src/main/scala/io/github/aaronreidsmith/year2024/Day15.scala
+++ src/main/scala/io/github/aaronreidsmith/year2024/Day15.scala
@@ -44,8 +44,7 @@ object Day15 extends Solution {
         case head :: tail =>
           val boxesToMove = findBoxesToMove(head, List(robot))
           val updated = boxesToMove.foldLeft(state) { (acc, point) =>
-            val char = state(point)
-            acc.updated(point.move(head), char).updated(point, '.')
+            acc ++ Map(point.move(head) -> state(point), point -> '.')
           }
           helper(updated, tail)
       }
@@ -54,7 +53,61 @@ object Day15 extends Solution {
     helper(rawGrid.toGrid, initialInstructions)
   }
 
+  // Adapted from https://github.com/sim642/adventofcode/blob/e6f4e3be68f4838677ba582dd55c833a86b1004a/src/main/scala/eu/sim642/adventofcodelib/GridImplicits.scala
   override def part2(input: (String, List[Direction])): Int = {
-    ???
+    val vertical                = Set(Direction.North, Direction.South)
+    val (rawGrid, instructions) = input
+    val initialGrid = rawGrid.flatMap {
+      case '#' => "##"
+      case 'O' => "[]"
+      case '.' => ".."
+      case '@' => "@."
+      case c   => c.toString
+    }.toGrid
+    val initialRobot = initialGrid.collectFirst { case (p, c) if c == '@' => p }.get
+
+    def move(state: Grid[Char], robot: Point, direction: Direction): (Grid[Char], Point) = {
+      def helper(grid: Grid[Char], pos: Point): Option[Grid[Char]] = {
+        grid(pos) match {
+          case '.' => Some(grid)
+          case '#' => None
+          case '[' if vertical.contains(direction) =>
+            val newPos = pos.move(direction)
+            for {
+              newGrid  <- helper(grid, newPos)
+              newGrid2 <- helper(newGrid, newPos.right)
+            } yield {
+              newGrid2
+                .updated(newPos, '[')
+                .updated(newPos.right, ']')
+                .updated(pos, '.')
+                .updated(pos.right, '.')
+            }
+          case ']' if vertical.contains(direction) =>
+            helper(grid, pos.left)
+          case cell @ ('[' | ']') if !vertical.contains(direction) =>
+            val newPos = pos.move(direction)
+            helper(grid, newPos).map { newGrid =>
+              newGrid
+                .updated(newPos, cell)
+                .updated(pos, '.')
+            }
+          case _ => throw IllegalArgumentException()
+        }
+      }
+
+      val newRobot = robot.move(direction)
+      helper(state, newRobot).fold((state, robot))((_, newRobot))
+    }
+
+    instructions
+      .foldLeft((initialGrid.updated(initialRobot, '.'), initialRobot)) {
+        case ((grid, robot), direction) => move(grid, robot, direction)
+      }
+      ._1
+      .foldLeft(0) {
+        case (acc, (point, '[')) => acc + (point.x * 100 + point.y)
+        case (acc, _)            => acc
+      }
   }
 }
--- src/test/scala/io/github/aaronreidsmith/year2024/Day15Test.scala
+++ src/test/scala/io/github/aaronreidsmith/year2024/Day15Test.scala
@@ -3,5 +3,5 @@ package io.github.aaronreidsmith.year2024
 import io.github.aaronreidsmith.BaseTest
 
 class Day15Test extends BaseTest {
-  val suite: Suite = Suite(fileInput, 10092)
+  val suite: Suite = Suite(fileInput, 10092, 9021)
 }
